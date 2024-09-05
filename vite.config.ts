e7e0430a3e92565f9a569528564367b2a9d56e57
Mohit <mohit@Mohits-MacBook-Pro-3.local>
2024-09-06 02:13:15 +0530

    minor fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import svgrPlugin from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/presentation-template",
+  base:"/",
   plugins: [
     react(),
     svgrPlugin({
